b90049d7a44c17475d50dfaaf3700291cca6219b
Roman <romires2000@gmail.com>
2020-06-02 18:15:41 +0300

    fix

--- php/admin.php
+++ php/admin.php
@@ -7,8 +7,8 @@ if(!isset($_COOKIE['admin'])){
 <head>
     <meta charset="utf-8">
     <title>admin room</title>
-    <script src="/js/main.js"></script>
-    <script src="/js/mainA.js"></script>
+    <script src="../js/main.js"></script>
+    <script src="../js/mainA.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
